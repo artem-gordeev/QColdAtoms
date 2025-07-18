86679c2bd001148116a385195a36773ea82858d2
artem-gordeev <ar1emg0rdeev@gmail.com>
2025-07-18 23:12:19 +0300

    11

--- src/ColdAtoms.jl
+++ src/ColdAtoms.jl
@@ -23,7 +23,7 @@ export
     HG_coeff, simulation_blue_intens,
     g1, p1, r1, gt1, zero1,
     Id, two_atom_simulation,
-    direct_CZ_simulation
+    direct_CZ_simulation1
         
 include("utilities.jl")
 include("basic_experiments.jl")
